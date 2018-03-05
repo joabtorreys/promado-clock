bfe1702a607f7aad1ef4c2ce890b9b6a1c788f61
iamjoey <j.torreys@outlook.com>
2018-03-05 11:39:42 -0400

    got everything working

--- script.js
+++ script.js
@@ -1,69 +1,78 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-var count1 = parseInt($('#num1').html());
-var count2= parseInt($('#num2').html());
+  var count1 = parseInt($('#num1').html());
+  var count2 = parseInt($('#num2').html());
 
-$("#add").click(function () {
+  $("#add").click(function() {
 
-  if (count1 < 100) {
+    if (count1 < 100) {
 
-    count1 += 5;
+      count1 += 1;
 
-  }
-  $("#num1").html(count1);
-});
-// grab add.
-$("#takeaway").click(function () {
+    }
+    $("#num1").html(count1);
+  });
+  // grab add.
+  $("#takeaway").click(function() {
 
-  if (count1 > 5) {
+    if (count1 > 0) {
 
-    count1 -= 5;
-  }
-  $("#num1").html(count1);
-});
-// grab takeaway.
+      count1 -= 1;
+    }
+    $("#num1").html(count1);
+  });
+  // grab takeaway.
 
-$("#add2").click(function () {
+  $("#add2").click(function() {
 
-  if (count2 < 100) {
+    if (count2 < 100) {
 
-    count2 += 5;
-  }
-  $("#num2").html(count2);
-});
-// grab next add.
-$("#takeaway2").click(function () {
+      count2 += 1;
+    }
+    $("#num2").html(count2);
+  });
+  // grab next add.
+  $("#takeaway2").click(function() {
 
-  if (count2 > 5) {
+    if (count2 > 0) {
 
-    count2 -= 5;
-  }
-  $("#num2").html(count2);
-});
-// grab next takeaway.
+      count2 -= 1;
+    }
+    $("#num2").html(count2);
+  });
+  // grab next takeaway.
 
 
-$('#start').click(function () {
-
-runner = setInterval(function () {
-if (count1 > 5) {
-  $("#add, #takeaway, #start").hide();
-  count1 -= 1;
-  $("#num1").html(count1);
-
-}else{
+  $('#start').click(function() {
+cnt = 0;
+    runner = setInterval(function() {
+      if (count1 > 0) {
+        $("#add, #takeaway, #start, #add2, #takeaway2").hide();
+        count1 -= 1;
+        $("#num1").html(count1);
+        cnt ++;
+if (cnt === count2 && count1 !== 0) {
   $('#thunder').trigger('play');
-  clearInterval(runner);
+  cnt = 0;
 }
+      } else {
+        $('#end').trigger('play');
+        clearInterval(runner);
+      }
 
 
-}, 1000);});
+    }, 1000);
+
+
+  });
+// grag the start button.
+
+
+$("#reset").click(function () {
+  location.reload();
+})
 
 
 
-
-
-
-
-//end.
+  //end.
 });
